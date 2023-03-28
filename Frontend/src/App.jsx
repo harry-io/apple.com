ddaf1dfa2c5a18a429cec623070f55c4410d725f
Arun Kumar ( Katill_Boyy ) <arun07744@gmail.com>
2023-03-29 02:06:00 +0530

    Apple Landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Navbar } from "../Components/Navbar";
-import Home from "../Pages/Home/Home";
+import { Navbar } from "./Components/Navbar";
+import Home from "./Pages/Home/Home";
 const AllRoutes = () => {
   return (
     <Routes>
