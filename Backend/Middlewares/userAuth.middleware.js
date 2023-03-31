75269edce3287bc9bca1555575d1fc3ab19bbc2c
Shivendra Singh <shivendrapaliwal99990@gmail.com>
2023-04-01 03:54:15 +0530

    Users_Products_Docs_Completed

--- Backend/Middlewares/userAuth.middleware.js
+++ Backend/Middlewares/userAuth.middleware.js
@@ -2,7 +2,7 @@ var jwt = require('jsonwebtoken');
 require("dotenv").config()
 
 const auth = (req,res,next) =>{
-  const token = req.headers.authorization
+  const token = req.headers?.authorization?.split(" ")[1]
   if(token){
   jwt.verify(token, process.env.secret_code, function(err, decoded) {
     if(decoded){
