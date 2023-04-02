f5497519e9342a30378ad380605578d645ee3bab
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-04-02 13:00:51 +0530

    debug

--- Frontend/src/Pages/Payment/Payment.jsx
+++ Frontend/src/Pages/Payment/Payment.jsx
@@ -3,8 +3,18 @@ import "../../Styles/Payment.scss";
 import { Link } from "react-router-dom";
 import { Navbar } from "../../Components/Navbar";
 
-const address = JSON.parse(localStorage.getItem("shipping"));
+const address = JSON.parse(localStorage.getItem("shipping"))||{
+  address1:"",
+  address2:"",
+  city:"",
+  pincode:"",
+  statename:"",
+  country:""
+
+};
 console.log(address);
+
+
 
 function Payment() {
   const [card, setCards] = useState("none");
